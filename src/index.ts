f6b97aedc9b9b3819d0207ee79461bbc7c8b2285
Half-Shot <will@half-shot.uk>
2022-09-01 09:46:01 +0100

    Add support for dark themes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { startWidget } from "./widget";
 
 window.onload = () => {
+    const search = new URLSearchParams(window.location.search);
+    const clientTheme = search.get("clientTheme");
+    document.querySelector('body')?.setAttribute('class', `theme-${clientTheme}`);
+
     startWidget(
         document.getElementsByTagName('main')[0],
-        new URLSearchParams(window.location.search),
+        search,
     )
 }
